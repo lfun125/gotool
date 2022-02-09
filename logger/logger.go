510bdacdaecf2e48d34b0c611c017f943d29b94d
lfun125 <lfun125@gmail.com>
2022-02-09 15:35:45 +0800

    logger

--- logger/logger.go
+++ logger/logger.go
@@ -22,6 +22,12 @@ type Interface interface {
 	Error(args ...interface{})
 	Panic(args ...interface{})
 	Fatal(args ...interface{})
+	Debugf(template string, args ...interface{})
+	Infof(template string, args ...interface{})
+	Warnf(template string, args ...interface{})
+	Errorf(template string, args ...interface{})
+	Panicf(template string, args ...interface{})
+	Fatalf(template string, args ...interface{})
 }
 
 type Logger struct {
@@ -88,9 +94,17 @@ func (l *Logger) Debug(args ...interface{}) {
 	l.getZapLogger().With(l.getArgs()...).Debug(args...)
 }
 
+func (l *Logger) Debugf(template string, args ...interface{}) {
+	l.getZapLogger().With(l.getArgs()...).Debugf(template, args...)
+}
+
 // Info uses fmt.Sprint to construct and log a message.
 func (l *Logger) Info(args ...interface{}) {
 	l.getZapLogger().With(l.getArgs()...).Info(args...)
+}
+
+func (l *Logger) Infof(template string, args ...interface{}) {
+	l.getZapLogger().With(l.getArgs()...).Infof(template, args...)
 }
 
 // Warn uses fmt.Sprint to construct and log a message.
@@ -98,9 +112,17 @@ func (l *Logger) Warn(args ...interface{}) {
 	l.getZapLogger().With(l.getArgs()...).Warn(args...)
 }
 
+func (l *Logger) Warnf(template string, args ...interface{}) {
+	l.getZapLogger().With(l.getArgs()...).Warnf(template, args...)
+}
+
 // Error uses fmt.Sprint to construct and log a message.
 func (l *Logger) Error(args ...interface{}) {
 	l.getZapLogger().With(l.getArgs()...).Error(args...)
+}
+
+func (l *Logger) Errorf(template string, args ...interface{}) {
+	l.getZapLogger().With(l.getArgs()...).Errorf(template, args...)
 }
 
 // Panic uses fmt.Sprint to construct and log a message, then panics.
@@ -108,9 +130,17 @@ func (l *Logger) Panic(args ...interface{}) {
 	l.getZapLogger().With(l.getArgs()...).Panic(args...)
 }
 
+func (l *Logger) Panicf(template string, args ...interface{}) {
+	l.getZapLogger().With(l.getArgs()...).Panicf(template, args...)
+}
+
 // Fatal uses fmt.Sprint to construct and log a message, then calls os.Exit.
 func (l *Logger) Fatal(args ...interface{}) {
 	l.getZapLogger().With(l.getArgs()...).Fatal(args...)
+}
+
+func (l *Logger) Fatalf(template string, args ...interface{}) {
+	l.getZapLogger().With(l.getArgs()...).Fatalf(template, args...)
 }
 
 func (l *Logger) getZapLogger() *zap.SugaredLogger {
